81a0b371dd9e2138c9c3deef9a82c4331a27466b
hamatatu <67685642+hamatatu@users.noreply.github.com>
2020-10-01 20:37:57 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,3 +5,36 @@ $(function () {
     $(id).show("fast");
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  var markerText = document.querySelectorAll('.js-marker'); // 監視対象を選択
+  var markerTextArr = Array.prototype.slice.call(markerText); // 監視対象をArrayに変換（IE対策）
+
+  /* IntersectionObserverに渡すコールバック関数 */
+  var cb = function(entries, observer) {
+    entries.forEach((entry) => {
+
+      if(entry.isIntersecting) {
+        /* 監視対象が画面内に入ったときのアクション */
+        entry.target.classList.add('inview'); // 画面内に入った要素にinviewクラスを付与
+        observer.unobserve(entry.target); // 1度発火した後監視を止める
+      }
+
+    });
+  }
+  
+  /* IntersectionObserverに渡すコールバック関数 */
+  var options = {
+    rootMargin: "-10px 0px"
+  }
+
+  /* IntersectionObserver初期化 */
+  var io = new IntersectionObserver(cb, options);
+  io.POLL_INTERVAL = 100; // Polyfillの設定
+
+  markerTextArr.forEach(el => {
+    io.observe(el);
+  });
+
+});
